bfd7fc104264fa4a6cddb4172b8671e31e9ef667
Rens Hamburger <renshamburger@gmail.com>
2017-12-23 21:48:23 +0100

    Things

---
+++ Config_template.php
@@ -0,0 +1,18 @@
+<?php
+
+class Config
+{
+    public const API_KEY = '';
+    public const USERNAME = '';
+    public const ADMIN_USERS = [];
+    public const COMMAND_PATH = [
+        __DIR__ . '/Commands/',
+    ];
+
+    public const MYSQL = [
+        'host' => 'localhost',
+        'user' => '',
+        'password' => '',
+        'database' => '',
+    ];
+}
